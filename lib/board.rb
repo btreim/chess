086337e735abf5acbb7393a3354de0b67f887514
btreim <btreim@gmail.com>
2018-05-01 11:02:04 -0500

    add pawn objects to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
+require_relative 'pawn'
+
 class Board
 	attr_reader :cell
 
 	def initialize
 		create_cells
+		create_pieces
 	end
 
 	def create_cells
@@ -10,4 +13,22 @@ class Board
 		0.upto(7) { |a| 0.upto(7){ |b| @cell << [a,b]}}
 	end
 
+	def create_pieces
+		@cell.each do |cell|
+			if cell[0] == 1 # Create Black Pawns
+				cell << Pawn.new(:black,([cell[0],cell[1]]))
+			elsif cell[0] == 6 # Create White Pawns
+				cell << Pawn.new(:white,([cell[0],cell[1]]))
+			end
+		end
+	end
+
 end
+
+b = Board.new
+
+b.cell.each_with_index do |cell, index|
+	puts "This is the cell: #{cell} and this is the index #{index}"
+end
+
+# p b.cell[2][1]
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,10 +2,21 @@ require 'board'
 
 describe "Board" do 
 	let(:board) { Board.new }
-	context "when initialized" do 
+	describe "when initialized" do 
+
 		it "creates an 8x8 grid" do 
 			expect(board.cell.size).to eql(64)
 		end
-	end 
+
+		it "positions :black pawns at row 2" do
+			expect(board.cell[8][2]).to be_an_instance_of(Pawn)
+		end	
+		
+		it "positions :white pawns at row 7" do
+			expect(board.cell[48][2]).to be_an_instance_of(Pawn)
+		end
+	
+	end
 end
 
+	
